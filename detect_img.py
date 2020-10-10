86f5e61f3f5d550d7cbd33554949b6600e34ffb7
Abhinandan <apple@Abhinandans-MacBook-Pro.local>
2020-10-10 15:13:51 +0800

    made importable module

--- detect_img.py
+++ detect_img.py
@@ -1,23 +1,28 @@
 from imageai.Detection import ObjectDetection
 
-## Set up detector
-detector = ObjectDetection()
-detector.setModelTypeAsRetinaNet()
+class Detector():
+    def __init__(self):
+        ## Set up detector
+        self.detector = ObjectDetection()
+        self.detector.setModelTypeAsRetinaNet()
 
-# Load model
-model_path = 'resnet50_coco_best_v2.0.1.h5'
+        # Load model
+        model_path = 'resnet50_coco_best_v2.0.1.h5'
 
-detector.setModelPath(model_path)
-detector.loadModel()
+        detector.setModelPath(model_path)
+        detector.loadModel()
 
-# Set custom object detection for dogs and cats
-# For more options, see: https://github.com/OlafenwaMoses/ImageAI/tree/master/imageai/Detection#---custom-object-detection
-custom_objects = detector.CustomObjects(dog=True)
+        # Set custom object detection for dogs
+        # For more options, see: https://github.com/OlafenwaMoses/ImageAI/tree/master/imageai/Detection#---custom-object-detection
+        custom_objects = detector.CustomObjects(dog=True)
+    
+    def detect(self,input_path, output_path):
+        # Detect
+        detections = self.detector.detectCustomObjectsFromImage(input_image=input_path, output_image_path=output_path, custom_objects=custom_objects, minimum_percentage_probability=45)
 
-## Detect on image
-input_path = 'test1.jpg'
-output_path = 'result.png'
-
-# Detect
-detections = detector.detectCustomObjectsFromImage(input_image=input_path, output_image_path=output_path, custom_objects=custom_objects, minimum_percentage_probability=45)
-
+if __name__ == '__main__':
+    #define unit test case here
+    dogdetector=Detector()
+    image='test1.jpg'
+    result='result.png'
+    dogdetector.detect(input_path=image, output_path=result)
