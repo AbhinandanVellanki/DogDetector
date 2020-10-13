ed4d5409ceb7bc952781b15673a8e7aa38511118
frederictwc <frederictwc@live.com>
2020-10-13 18:08:06 +0800

    test at office

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ import numpy as np
 from initialize import cam_1,net
 
 def main():
-    print(1)
     ct = 0
     while True:
-        print(1)
         try:
             ct += 1
             ret_1 = cam_1.grab()
             if ct % 40 == 0:
                 ret_1, img_1 = cam_1.retrieve()
                 if not ret_1: break
-                bboxes = net.detect_objects(img_1)
-                if len(bboxes)>0:
-                    print(f'Number of dogs: {len(bboxes)}')
+                bboxes = net.detect(img_1)
+                
+                print(f'Number of dogs: {len(bboxes)}')
         except Exception as e:
             print(e)
 
