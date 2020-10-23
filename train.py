9b211c27513f37524f41aa70d2b103e1b4f97b80
AbhinandanVellanki <abhinandan.vellanki@gmail.com>
2020-10-23 16:58:14 +0800

    removed duplicate code

--- train.py
+++ train.py
@@ -1,25 +1,18 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 
+
 class Trainer():
     def __init__(self):
         self.trainer = DetectionModelTrainer()
         self.trainer.setModelTypeAsYOLOv3()
-<<<<<<< HEAD
-        self.trainer.setDataDirectory(data_directory="dog_dataset") #Set name of dataset directory
+        # Set name of dataset directory
+        self.trainer.setDataDirectory(data_directory="dog_dataset")
         self.trainer.setTrainConfig(object_names_array=['dog'], batch_size=4, num_experiments=10, train_from_pretrained_model='pretrained-yolov3.h5')
-
-=======
-        self.trainer.setDataDirectory(data_directory="dog_dataset") #Set name of dataset directory
-        self.trainer.setTrainConfig(object_names_array=['dog'], batch_size=4, num_experiments=10, train_from_pretrained_model='pretrained-yolov3.h5')
-        
->>>>>>> e809d0e799aa0f669064d423b93280cc101252ec
 
     def train(self):
         self.trainer.trainModel()
-        
-if __name__== '__main__':
-    trainer_obj=Trainer()
+
+
+if __name__ == '__main__':
+    trainer_obj = Trainer()
     trainer_obj.train()
-
-        
-
